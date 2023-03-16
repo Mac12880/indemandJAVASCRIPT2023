fa2f3dda0087174782b433323b1a2ade55272483
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-16 21:54:44 +0800

    while loop A.

--- Day4/main.js
+++ Day4/main.js
@@ -1,15 +1,29 @@
-//A
+//ForLoopA
 // for(let x = 0; x < 10; x++){
 //     console.log(x)
 //     }
 
-//B
+//ForLoopB
 // const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
 // for(let x = 0; x < nums.length;x++){
 // console.log(nums[x])
 // }
 
-//C
-for(let i = 20; i>= 11;i--){
-    console.log(i)
-    }
+//ForLoopC
+// for(let i = 20; i>= 11;i--){
+//     console.log(i)
+//     }
+
+//WhileLoopA
+let num = 0
+while(num < 10){
+console.log(num)
+num++;
+}
+
+//WhileLoopB
+// let num = 0;
+// while (num < 10) {
+// console.log(num);
+// num+=2;
+// }
