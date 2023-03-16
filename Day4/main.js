0b08c01925646b0d6f93a125a2013c3ba7c807fb
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-16 22:04:07 +0800

    FunctionE

--- Day4/main.js
+++ Day4/main.js
@@ -65,45 +65,77 @@
 //     console.log("Area: " + results[1]);
 //   }
 
-//FunctionD
-function generateRNumber(start, end) {
-    let random = Math.floor(Math.random() * (end - start + 1)) + start;
-    return random;
-  }
+//FunctionD wrong
+// function generateRNumber(start, end) {
+//     let random = Math.floor(Math.random() * (end - start + 1)) + start;
+//     return random;
+//   }
   
-  let start = prompt("Enter the start of the range:");
-  let end = prompt("Enter the end of the range:");
+//   let start = prompt("Enter the start of the range:");
+//   let end = prompt("Enter the end of the range:");
   
-  if (start === null || end === null || start === "" || end === "") {
-    console.log("You did not enter a valid range.");
-  } else {
-    start = parseInt(start);
-    end = parseInt(end);
-    }
-    if (isNaN(start) || isNaN(end)) {
-      console.log("You did not enter valid numbers.");
-    } else {
-      let random = generateRandomNumber(start, end);
-      console.log("Random number between " + start + " and " + end + ": " + random);
-    }
-      function generateRandomNumber(start, end) {
-    let random = Math.floor(Math.random() * (end - start + 1)) + start;
-    return random;
-  }
+//   if (start === null || end === null || start === "" || end === "") {
+//     console.log("You did not enter a valid range.");
+//   } else {
+//     start = parseInt(start);
+//     end = parseInt(end);
+//     }
+//     if (isNaN(start) || isNaN(end)) {
+//       console.log("You did not enter valid numbers.");
+//     } else {
+//       let random = generateRandomNumber(start, end);
+//       console.log("Random number between " + start + " and " + end + ": " + random);
+//     }
+//       function generateRandomNumber(start, end) {
+//     let random = Math.floor(Math.random() * (end - start + 1)) + start;
+//     return random;
+//   }
   
-  let start = prompt("Enter the start of the range:");
-  let end = prompt("Enter the end of the range:");
+//   let start = prompt("Enter the start of the range:");
+//   let end = prompt("Enter the end of the range:");
   
-  if (start === null || end === null || start === "" || end === "") {
-    console.log("You did not enter a valid range.");
-  } else {
-    start = parseInt(start);
-    end = parseInt(end);
-    if (isNaN(start) || isNaN(end)) {
-      console.log("You did not enter valid numbers.");
-    } else {
-      let random = generateRNumber(start, end);
-      console.log("Random number between " + start + " and " + end + ": " + random);
-    }
-  }
+//   if (start === null || end === null || start === "" || end === "") {
+//     console.log("You did not enter a valid range.");
+//   } else {
+//     start = parseInt(start);
+//     end = parseInt(end);
+//     if (isNaN(start) || isNaN(end)) {
+//       console.log("You did not enter valid numbers.");
+//     } else {
+//       let random = generateRNumber(start, end);
+//       console.log("Random number between " + start + " and " + end + ": " + random);
+//     }
+//   }
+
 //FunctionE
+function factorial(num) {
+    if (num < 0) {
+      return undefined;
+    } else if (num === 0 || num === 1) {
+      return 1;
+    } else {
+      let result = 1;
+      for (let i = 2; i <= num; i++) {
+        result *= i;
+      }
+      return result;
+    }
+  }
+  
+  let input = prompt("Enter a number:");
+  
+  if (input === null || input === "") {
+    console.log("You did not enter a valid number.");
+  } else {
+    input = parseInt(input);
+    if (isNaN(input)) {
+      console.log("You did not enter a valid number.");
+    } else {
+      let result = factorial(input);
+      if (result === undefined) {
+        console.log("Factorial is undefined for negative numbers.");
+      } else {
+        console.log("Factorial of " + input + " is " + result);
+      }
+    }
+  }
