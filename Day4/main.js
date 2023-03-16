5811123e000e8e7869b524e007ceabce990e2612
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-16 21:43:43 +0800

    for loop B.

--- Day4/main.js
+++ Day4/main.js
@@ -1,3 +1,10 @@
-for(let x = 0; x < 10; x++){
-    console.log(x)
-    }
+//A
+// for(let x = 0; x < 10; x++){
+//     console.log(x)
+//     }
+
+//B
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
+for(let x = 0; x < nums.length;x++){
+console.log(nums[x])
+}
