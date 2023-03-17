f65a116a3752b99f1de119766113b5cb746c29b2
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-17 16:24:44 +0800

    JS Function1

--- Day5/main.js
+++ Day5/main.js
@@ -1,15 +1,27 @@
 // Define an object called circle which will hold methods that have to 
 //do with the geometry of circle it should contain two methods: Perimeter and Area
 
-const circle = {
-  perimeter: function(radius){
-      return (2 * Math.PI * radius);
-      console.log();
+// const circle = {
+//   perimeter: function(radius){
+//       return (2 * Math.PI * radius);
+//       console.log();
+//   },
+//   area: function(radius){
+//       return (Math.PI * radius * radius);
+//   }
+// }
+// circle.perimeter(3);
+
+//JSFunction1
+const square = {
+  area: function(side){
+    return side*side;
   },
-  area: function(radius){
-      return (Math.PI * radius * radius);
+  perimeter: function(side){
+    return side+side+side+side
   }
 }
-circle.perimeter(3);
 
-
+let size = Number(prompt("Enter Size"))
+console.log(`Area = ${square.area(size)}`);
+console.log(`Perimeter = ${square.perimeter(size)}`);
