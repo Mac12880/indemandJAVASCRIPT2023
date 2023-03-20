1e290cc8da9b2da0365a26d150b7c539d6bc821e
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-20 15:06:14 +0800

    JS Set Timeout

--- Day6/main.js
+++ Day6/main.js
@@ -35,25 +35,33 @@
 // const add = (a, b) => a+b;
 
 
-const isEven = function(num) { // Regular Function Expressions 
-  return num % 2 === 0;
-}
+// const isEven = function(num) { // Regular Function Expressions 
+//   return num % 2 === 0;
+// }
 
-const isEven = (num) => { // Arrow Function with parenthesis around the argument
-  return num % 2 === 0;
-}
+// const isEven = (num) => { // Arrow Function with parenthesis around the argument
+//   return num % 2 === 0;
+// }
 
-const isEven = num => { // Arrow Function with no parenthesis around the argument(for single argument only)
-  return num % 2 === 0;
-}
+// const isEven = num => { // Arrow Function with no parenthesis around the argument(for single argument only)
+//   return num % 2 === 0;
+// }
 
-const isEven = num => { // Implicit Return
-  num % 2 === 0
-}
+// const isEven = num => { // Implicit Return
+//   num % 2 === 0
+// }
 
-const isEven = num => num % 2 === 0; // One-liner with Implicit Return
+// const isEven = num => num % 2 === 0; // One-liner with Implicit Return
 
+// Set Timeout
+setTimeout(() => {
+  console.log("Hello!");
+  setTimeout(() => {
+    console.log("Bye!");
+  }, 3000);
+}, 3000);
 
+// Set Interval
 
 
 
