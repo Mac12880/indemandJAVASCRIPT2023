adce218761243cf3af7c2ec5a42ffc62f4ef580e
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-20 14:53:36 +0800

    Implicit Return

--- Day6/main.js
+++ Day6/main.js
@@ -1,28 +1,61 @@
 //map
-const fullName = [
-  {
-      first: "Martin",
-      last: "Juan"
-  },
-  {
-      first: "Kyle",
-      last: "Reyes"
-  },
-  {
-      first: "Aaron",
-      last: "Rubio"
-  },
-  {
-      first: "Mark",
-      last: "Balala"
-  },
-  {
-      last: "Samolde"
-  },
-];
+// const fullName = [
+//   {
+//       first: "Martin",
+//       last: "Juan"
+//   },
+//   {
+//       first: "Kyle",
+//       last: "Reyes"
+//   },
+//   {
+//       first: "Aaron",
+//       last: "Rubio"
+//   },
+//   {
+//       first: "Mark",
+//       last: "Balala"
+//   },
+//   {
+//       last: "Samolde"
+//   },
+// ];
 
-const firstName = fullName.map(function(name){
-  return name.first;
-})
+// const firstName = fullName.map(function(name){
+//   return name.first;
+// })
 
-console.log(firstName);
+// console.log(firstName);
+
+// lesser
+// const string = (str) => {str};
+
+// Implicit return
+// const string = (str) => str;
+// const add = (a, b) => a+b;
+
+
+const isEven = function(num) { // Regular Function Expressions 
+  return num % 2 === 0;
+}
+
+const isEven = (num) => { // Arrow Function with parenthesis around the argument
+  return num % 2 === 0;
+}
+
+const isEven = num => { // Arrow Function with no parenthesis around the argument(for single argument only)
+  return num % 2 === 0;
+}
+
+const isEven = num => { // Implicit Return
+  num % 2 === 0
+}
+
+const isEven = num => num % 2 === 0; // One-liner with Implicit Return
+
+
+
+
+
+
+
