096c413a5b740c1de1986fb281b0b637857420f3
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-21 16:04:14 +0800

    Default Parameter and Spread Operator #1

--- Day7/main.js
+++ Day7/main.js
@@ -1,25 +1,34 @@
 // practice array.reduced
 
-const animes = [
-  {
-    title: "Naruto",
-    score: 100
-  },
-  {
-    title: "Capeta",
-    score: 98
-  },
-  {
-    title: "Hunter X Hunter",
-    score: 95
-  },
-  {
-    title: "Flame of Recca",
-    score: 90
-  }
-];
+// const animes = [
+//   {
+//     title: "Naruto",
+//     score: 100
+//   },
+//   {
+//     title: "Capeta",
+//     score: 98
+//   },
+//   {
+//     title: "Hunter X Hunter",
+//     score: 95
+//   },
+//   {
+//     title: "Flame of Recca",
+//     score: 90
+//   }
+// ];
 
-const bestAnime = animes.reduce((highest, score) => {
-  return (score.score > highest.score) ? score : highest;
-}, animes[0]);
-console.log(bestAnime)
+// const bestAnime = animes.reduce((highest, score) => {
+//   return (score.score > highest.score) ? score : highest;
+// }, animes[0]);
+// console.log(bestAnime)
+
+//Default Parameter and Spread Operator #1
+
+const sum = (x = 5, y = 10) => {
+  return x + y;
+};
+console.log(sum());
+console.log(sum(5));
+console.log(sum(10,10));
