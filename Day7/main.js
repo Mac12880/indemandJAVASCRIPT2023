b3afbb6c5eb36f9e6825e972b055fd1b6d457903
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-21 13:53:05 +0800

    array

---
+++ Day7/main.js
@@ -0,0 +1,20 @@
+// practice array.reduced
+
+const animes = [
+  {
+    title: "Naruto",
+    score: 100
+  },
+  {
+    title: "Capeta",
+    score: 98
+  },
+  {
+    title: "Hunter X Hunter",
+    score: 95
+  },
+  {
+    title: "Flame of Recca",
+    score: 90
+  }
+];
