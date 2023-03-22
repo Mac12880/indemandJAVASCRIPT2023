37d9f9c51117da7fcf46c3faa5cd2afd4729c89b
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-22 16:55:26 +0800

    DOM Exercise 6

--- Day8/main.js
+++ Day8/main.js
@@ -11,7 +11,13 @@
 // const img = document.querySelector("img").src = "JavaScript-logo.png";
 
 // DOM Exercise 5
-const h1 = document.querySelector("h1");
-h1.style.color = "yellow";
-h1.style.textAlign = "center";
-h1.style.border = "5px dotted black";
+// const h1 = document.querySelector("h1");
+// h1.style.color = "yellow";
+// h1.style.textAlign = "center";
+// h1.style.border = "5px dotted black";
+
+// DOM Exercise 6
+const h1 = document.querySelector("#heading2");
+h1.style.display = "none";
+
+
