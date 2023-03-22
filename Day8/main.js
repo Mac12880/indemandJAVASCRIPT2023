9697abe370f8498552bed27532165af308461d90
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-22 16:51:19 +0800

    DOM Exercise 2

--- Day8/main.js
+++ Day8/main.js
@@ -1,2 +1,6 @@
 // DOM Exercise 1
-const elem = document.querySelector("#para").innerHTML = "<elem>Hello, World!</elem>";
+// const elem = document.querySelector("#para").innerHTML = "<elem>Hello, World!</elem>";
+
+// DOM Exercise 2
+const img3 = document.getElementById("img3");
+
