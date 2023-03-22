68fac6c223f5cc0fcfb6fe65309e37e17e411f6f
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-22 16:53:24 +0800

    DOM Exercise 4

--- Day8/main.js
+++ Day8/main.js
@@ -5,7 +5,11 @@
 // const img3 = document.getElementById("img3");
 
 // DOM Exercise 3
-const par = document.querySelector("p").innerHTML = "Hello, World!";
+// const par = document.querySelector("p").innerHTML = "Hello, World!";
+
+// DOM Exercise 4
+const img = document.querySelector("img").src = "JavaScript-logo.png";
+
 
 
 
