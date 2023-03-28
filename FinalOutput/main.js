b5b6e9f49d23d655e3f0fadf3c694b73cf83078d
Mac12880 <marc.juan6.mj@gmail.com>
2023-03-28 18:10:22 +0800

    todo

--- FinalOutput/main.js
+++ FinalOutput/main.js
@@ -1,74 +1,30 @@
 const submit = document.querySelector("#submit");
-const ul = document.querySelector("#list")
+// const ul = document.querySelector("#list");
+const todoListElem = document.querySelector("#list");
 
 
-// function addTodo(){
-//     const textBox = document.querySelector("#toDoInput").value
-//     if(textbox != null){
-//         allToDoList.push(textBox);
-//         console.log
-//         const ul = document.querySelector("ul");
-//         const li = document.createElement("li");
-//         li.textContent = items;
-//         ul.appendChild(li);
-//     }
-    
-    
-// }
-// let activeTodoList = [];
-// let completedTodoList = [];
+
 submit.addEventListener("click", function (event) {
     event.preventDefault();
     addTodo();
+    showTodo();
 });
 
-
-// function addTodo(){
-//     let toDo = document.querySelector("#toDoInput");
-//     console.log(toDo.value);
-//     if(toDo.value == ""){ // add remove similar todo 
-//         alert("You didn't enter a task!");
-//     }
-//     else{
-//         activeTodoList.push(toDo.value)
-//         console.log(activeTodoList);
-//         ul.innerHTML = ul.innerHTML + 
-//         `<li><input type="checkbox" id="complete">${toDo.value}<button name="delete" value="delete">X</button></li>`;
-//         toDo.value = "";
-
-
-
-//     }
-        
-// }
-
-// const complete = document.querySelector("#complete");
-// complete.addEventListener("click", completeTodo);
-
-// function completeTodo(){
-//     let del = document.querySelector(`#complete${}`)
-//     if(value == activeTodoList.find(value)){
-
-//     }
-    
-// }
-
-
-const id = [];
+// const id = [];
 const activeTodoList = [];
-const completedTodoList = [];
+// const completedTodoList = [];
 let count;
 
 function addTodo(){
     let toDo = document.querySelector("#toDoInput");
     console.log(`Inputted text: ${toDo.value}`);
-    toDo.value = toDo.value.trimStart() 
-    // const isDuplicate = activeTodoList.some((todos) => todos.value == toDo.value);
+
     if(toDo.value === ""){
         alert("You didn't enter a task!");
         toDo.value = "";
     }
-    else if(activeTodoList.includes(toDo.value)){
+    //to check if toDo.value is a duplicate on an array of objects
+    else if(activeTodoList.some(({value}) => value === `${toDo.value}`)){
         alert("You have inputted a duplicated task!");
         toDo.value = "";
     }
@@ -78,50 +34,86 @@ function addTodo(){
     }
     else{
         activeTodoList.push({
-            value: toDo.Value,
+            value: toDo.value,
             checked: false,
         });
+        console.log(toDo.value);
         toDo.value = "";
-
-
-        
-        // count = id.length + 1;
-        // id.push(count);
-        // console.log(`Show ID array: ${id}`);
-        // // activeTodoList.push(`${count}-${toDo.value}`);
-        // activeTodoList.push(`${toDo.value}`);
-        // // console.log(activeTodoList);
-        // ul.innerHTML = ul.innerHTML + `<li id="${count}">${toDo.value}<button name="complete" value="complete" id="complete${count}">Complete</button><button name="delete" value="delete" id"delete${count}">Delete</button></li>`;
-        // toDo.value = "";
     }
-        console.log(`Active to do List Array: ${activeTodoList}`);    
+        console.log(`Active to do List Array: ${activeTodoList[0]} ${activeTodoList[1]}`);    
 }
 
+function showTodo(){
+    todoListElem.innerHTML = "";//Reset element 
+    activeTodoList.forEach((todo, index) => {
+        
 
+        //show on html
+        todoListElem.innerHTML += `
+        <div class="toDo" id="${index}">
+            <table>
+                <tr>
+                    <td class="left">
+                        <a><i class="bi ${todo.checked ? 'bi-check-circle' : 'bi-circle'}"
+                        data-action="check">
+                        
+                        </i></a>
+                        <p style="display: inline-block;" data-action="check">${todo.value}</p>
+                    </td>
+                    <td class="right">
+                        <a><i class="bi bi-trash3-fill" data-action="delete"></i></a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        `;
+        console.log(activeTodoList[0]);
+    });
+}
 
-// const doneTodo = document.querySelector(".complete");
-// count = doneTodo.parentNode.id;
-// if(doneTodo.value == true){
+//event listener for all todo's
+todoListElem.addEventListener('click', (event) =>{
+    const target = event.target;
+    let parentElem = target;
+    //get the GreatGreatGreatGrandParent
+    for (let i = 0; i < 6; i++) {
+    parentElem = parentElem.parentNode;
+    }
+
+    if(parentElem.className !== 'toDo'){
+        return
+    }
+    // console.log(parentElem)//show the GreatGreatGreatGrandParent node
+    const todo = parentElem;
+    const todoId = Number(todo.id);
+    console.log(todoId);//show id
     
-// }
-// function MoveUL(){
-//     listwo.append(this);
-//     this.removeEventListener("click", MoveUL);
-// }
-// li.forEach( (el) => {
-// el.addEventListener("click", MoveLi);
-// });
+    //target action
+    const action = target.dataset.action
+    // console.log(todoId, action);//check the action
 
-activeTodoList.forEach((element) => {
-    console.log(`Element in active array: ${element}`)
+    action === "check" && checkTodo(todoId);
+    // action === "delete" && deleteTodo(todoId);
 
+    
 });
 
-activeTodoList.forEach(checkActiveTodo);
+//Check a todo
 
-
-function checkActiveTodo(item){
-    console.log("Test")
+function checkTodo(){
+    activeTodoList.map
 }
+
+// activeTodoList.forEach((element) => {
+//     console.log(`Element in active array: ${element}`)
+
+// });
+
+// activeTodoList.forEach(checkActiveTodo);
+
+
+// function checkActiveTodo(item){
+//     console.log("Test")
+// }
 
 
